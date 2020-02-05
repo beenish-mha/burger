89cdaf42eb3ca21e772744cfdb6b8ea96ea429e6
beenish <beeni.khurram@gmail.com>
2020-02-05 14:25:27 +0000

    created db and table in schema and seed files

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE DATABASE burgers_db;
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN 
+)
---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+USE DATABASE burgers_db;
+
+INSERT INTO burgers (burger_name)
+    VALUES("Chicken Burger");
+
+INSERT INTO burgers (burger_name)
+    VALUES("Beef Burger");
+
+INSERT INTO burgers(burger_name)
+    VALUES("Vegi Supreme Burger");
